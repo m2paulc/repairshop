ddd38d125dbf66ba3e28a0a33356e4ea77e20107
m2paulc <39387011+m2paulc@users.noreply.github.com>
2025-02-03 11:47:32 -0500

    create card ui for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function AdminPage() {
-  return <div>Admin Page</div>;
+  return (
+    <>
+      <Card className="w-full border-none bg-gray-100 drop-shadow-md">
+        <CardHeader>
+          <CardTitle className="text-2xl">Admin</CardTitle>
+        </CardHeader>
+      </Card>
+    </>
+  );
 }
