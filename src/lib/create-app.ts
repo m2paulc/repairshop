dfe9ab2a4cd4e5a0088375e2e5f135ee64f29180
m2paulc <39387011+m2paulc@users.noreply.github.com>
2025-02-05 11:02:05 -0500

    organize files for usability

---
+++ src/lib/create-app.ts
@@ -0,0 +1,22 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { AppBindings } from "./types";
+import { defaultHook } from "stoker/openapi";
+import { notFound, onError } from "stoker/middlewares";
+import { pinoLogger } from "@/middlewares/pino-logger";
+
+export function createRouter() {
+  return new OpenAPIHono<AppBindings>({ strict: false, defaultHook }).basePath(
+    "/api"
+  );
+}
+
+export default function createApp() {
+  const app = createRouter();
+
+  app.use(pinoLogger());
+
+  app.notFound(notFound);
+  app.onError(onError);
+
+  return app;
+}
