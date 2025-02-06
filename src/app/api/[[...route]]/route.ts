4d20bf2fdd6ef152ffa1ce1f647252a33ffdc799
m2paulc <39387011+m2paulc@users.noreply.github.com>
2025-02-06 11:30:30 -0500

    refactor route.ts to fix 404 instances with openapi

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,21 +1,16 @@
 import { handle } from "hono/vercel";
 import createApp from "@/lib/create-app";
 import configureOpenAPI from "@/lib/configure-open-api";
-import api from "./index.api";
 import tasks from "./(tasks)/tasks.index";
-
 export const runtime = "nodejs";
 
 const app = createApp();
 
 // documentation endpoint
 configureOpenAPI(app);
-// routes
-const routes = [api, tasks] as const;
 
-routes.forEach((route) => {
-  app.route("/", route);
-});
+// routes
+const routes = app.route("/api/tasks", tasks);
 
 export const GET = handle(app);
 export const POST = handle(app);
